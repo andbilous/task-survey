fb418b039e6acd571b7922cfb266afda5c94d846
andbilous-digis <andbilous@gmail.com>
2020-06-18 22:58:04 +0300

    Updates

---
+++ precache-manifest.ed3c598405aa32964e4fa7accc54d317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444273ae85d36a06e692a20a0f2dde2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11be21b4239a953f37ee",
+    "url": "/static/css/main.0b641c17.chunk.css"
+  },
+  {
+    "revision": "1a5fa03b866ad3d3c7a5",
+    "url": "/static/js/2.22b54a88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.22b54a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11be21b4239a953f37ee",
+    "url": "/static/js/main.cf0f63c1.chunk.js"
+  },
+  {
+    "revision": "a562bb0cd935e989fa51",
+    "url": "/static/js/runtime-main.5d02b201.js"
+  }
+]);
